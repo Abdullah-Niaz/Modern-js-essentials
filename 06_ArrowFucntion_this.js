3b61c9d2d093c62237374d6a6dddf27e3eb83455
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-22 23:44:42 +0500

    Ex of =>

--- 06_ArrowFucntion_this.js
+++ 06_ArrowFucntion_this.js
@@ -12,5 +12,18 @@ const squareshort = n => n * n;
 sqobj = squareshort(3);
 console.log(sqobj);
 
+const jobs = [
+    { id: 1, isActive: true },
+    { id: 2, isActive: false },
+    { id: 3, isActive: true },
+    { id: 4, isActive: false },
+]
 
+const activeJobs = jobs.filter(function (jobs) {
+    return jobs.isActive == true;
+})
 
+console.log(activeJobs)
+
+const ActiveJobs = jobs.filter(jobs => jobs.isActive == false);
+console.log(ActiveJobs);
