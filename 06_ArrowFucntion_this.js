1aba7a22dfc9bf1ed3f514ecbb4ce12c107acfb8
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-22 23:40:46 +0500

    different Styles of treating with arrow fucntions

---
+++ 06_ArrowFucntion_this.js
@@ -0,0 +1,16 @@
+const square = function (n) {
+    return n * n;
+}
+console.log(square(5));
+
+const squaren = (n) => {
+    return n * n;
+}
+console.log(squaren(2));
+
+const squareshort = n => n * n;
+sqobj = squareshort(3);
+console.log(sqobj);
+
+
+
