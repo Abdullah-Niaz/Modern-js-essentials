e8d12c3c6c80ca5c67d0b97b46364d4e8d2fad4d
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-23 12:32:10 +0500

    this behaviour

--- 06_ArrowFucntion_this.js
+++ 06_ArrowFucntion_this.js
@@ -1,29 +1,17 @@
-const square = function (n) {
-    return n * n;
+// const person = {
+//     talk() {
+//         var self = this;
+//         setTimeout(function () {
+//             console.log(self);
+//         }, 1000);
+//     }
+// }
+const person = {
+    talk() {
+        setTimeout(() => {
+            console.log(this);
+        }, 1000);
+    }
 }
-console.log(square(5));
-
-const squaren = (n) => {
-    return n * n;
-}
-console.log(squaren(2));
-
-const squareshort = n => n * n;
-sqobj = squareshort(3);
-console.log(sqobj);
-
-const jobs = [
-    { id: 1, isActive: true },
-    { id: 2, isActive: false },
-    { id: 3, isActive: true },
-    { id: 4, isActive: false },
-]
-
-const activeJobs = jobs.filter(function (jobs) {
-    return jobs.isActive == true;
-})
-
-console.log(activeJobs)
-
-const ActiveJobs = jobs.filter(jobs => jobs.isActive == false);
-console.log(ActiveJobs);
+r = person.talk();
+console.log(r);
